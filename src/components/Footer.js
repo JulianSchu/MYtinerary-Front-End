c58dc9c55ef97d6c8318b7fcb269c9a408a0c098
JulianSchu <jun.xu@hotmail.dem>
2019-06-07 11:20:33 +0200

    footer modified with disabled link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,30 @@ import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router-dom';
 
 export class Footer extends Component {
+    getClass = () => {
+        if (this.props.location.pathname === "/") {
+            return 'text-muted'
+        } else {
+            return 'text-dark'
+        }
+    }
+
+    getStyle = () => {
+        if (this.props.location.pathname === "/") {
+            // const disable = {
+            //     pointerEvents: 'none'  
+            // }
+            return {
+                pointerEvents: 'none'  
+            }
+        }
+    }
+
     render() {
         console.log(this.props)
         return (
             <div className="d-flex justify-content-center fixed-bottom">
-                <Link to="/" className="text-dark">
+                <Link to="/" style={this.getStyle()} className={this.getClass()} >
                     <h3><i className="fas fa-home"></i></h3>
                 </Link>
             </div>
